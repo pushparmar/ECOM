37e2e51921a0daab5f19ceaebc02d58dbc5f38ce
pushpendra <pushpendra.parmar@angara.in>
2019-12-06 16:40:13 +0530

    home page components create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import HomePage from './pages/homepage/homepgae-component';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
-}
+function App(){
+  return (
+    <div className='App'>
+      <HomePage/>
+    </div>
+  )
+} 
 
 export default App;
