40de87c06469e282fb7dc08fffdd21c060b08e4b
pushpendra <pushpendra.parmar@angara.in>
2019-12-07 18:31:40 +0530

    header component added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import ShopPage from './pages/shop/shop.component'
 import CollectionPreivew from './component/collection-preview/collection-preview.component';
 import './App.css';
 
+
+import Header from './component/header/header.component'
+
 const hats = ()=> (
     <div>its a hats page</div> 
 )
@@ -13,6 +16,7 @@ const hats = ()=> (
 function App(){
   return (
     <div>
+        <Header/>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
