8d74a0eed631dbefec2581db5f75a6c9c417c5f3
pushpendra <pushpendra.parmar@angara.in>
2020-01-02 14:34:43 +0530

    pwa

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './category.styles.scss';
+
+
+import CollectionItem from '../../component/collection-item/collection-item.component';
+import {filterShopData } from '../../redux/shop_data/shopData.selector';
+import { connect } from 'react-redux';
+
+const CategoryPage = ({ collection }) => {
+    console.log(collection  )
+    return (
+    <div className="category">
+        <h2>
+            CATEGORY PAGE
+        </h2>
+    </div>
+    )
+}
+
+
+const mapStateToProps = (state , ownProps) => ({
+    collection : filterShopData(ownProps.match.params.categoryId)(state)
+})
+export default connect(mapStateToProps)(CategoryPage);
